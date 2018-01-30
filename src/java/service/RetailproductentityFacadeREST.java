b3804878eeaebb1735c700b7d4213428e64aec03
Bryan K <Bryan K@172.22.48.140>
2018-01-30 17:38:45 +0800

    Added getRetailProductList method at webservice to retrieve from database

--- src/java/service/RetailproductentityFacadeREST.java
+++ src/java/service/RetailproductentityFacadeREST.java
@@ -86,5 +86,55 @@ public class RetailproductentityFacadeREST extends AbstractFacade<Retailproducte
     protected EntityManager getEntityManager() {
         return em;
     }
+    
+    @GET
+    @Path("getRetailProductList")
+    @Produces("application/json")
+    public Response getRetailProductList(@QueryParam("countryID") Long countryID) {
+        try {
+            List<RetailProduct> list = new ArrayList<>();
+            String stmt = "";
+            PreparedStatement ps;
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/islandfurniture-it07?zeroDateTimeBehavior=convertToNull&user=root&password=12345");
 
+            if (countryID == null) {
+                stmt = "SELECT i.ID as id, i.NAME as name, r.IMAGEURL as imageURL, i.SKU as sku, i.DESCRIPTION as description, i.TYPE as type, i._LENGTH as length, i.WIDTH as width, i.HEIGHT as height, i.CATEGORY as category FROM itementity i, retailproductentity r where i.ID=r.ID and i.ISDELETED=FALSE;";
+                ps = conn.prepareStatement(stmt);
+            } else {
+                stmt = "SELECT i.ID as id, i.NAME as name, r.IMAGEURL as imageURL, i.SKU as sku, i.DESCRIPTION as description, i.TYPE as type, i._LENGTH as length, i.WIDTH as width, i.HEIGHT as height, i.CATEGORY as category, ic.RETAILPRICE as price FROM itementity i, retailproductentity r, item_countryentity ic where i.ID=r.ID and i.ID=ic.ITEM_ID and i.ISDELETED=FALSE and ic.COUNTRY_ID=?;";
+                ps = conn.prepareStatement(stmt);
+                ps.setLong(1, countryID);
+            }
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                RetailProduct r = new RetailProduct();
+                r.setId(rs.getLong("id"));
+                r.setName(rs.getString("name"));
+                r.setImageUrl(rs.getString("imageURL"));
+                r.setSKU(rs.getString("sku"));
+                r.setDescription(rs.getString("description"));
+                r.setType(rs.getString("type"));
+                r.setCategory(rs.getString("category"));
+                if (countryID != null) {
+                    r.setPrice(rs.getDouble("price"));
+                }
+                list.add(r);
+            }
+            GenericEntity<List<RetailProduct>> entity = new GenericEntity<List<RetailProduct>>(list) {
+            };
+            return Response
+                    .status(200)
+                    .header("Access-Control-Allow-Origin", "*")
+                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                    .header("Access-Control-Allow-Credentials", "true")
+                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                    .header("Access-Control-Max-Age", "1209600")
+                    .entity(entity)
+                    .build();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
 }
